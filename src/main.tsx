73842fe53005d2f66d98df1c09e609aef5a74225
Rodrigo Silva de Sena <rodrigo.sena1163@gmail.com>
2024-10-01 13:59:41 -0400

    configuração inicial do useContext

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,12 @@ import { router } from './App.tsx'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
 
+import CartProvider from './contexts/CartContext.tsx'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
   </StrictMode>,
 )
