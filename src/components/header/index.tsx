eaf6532505ac9f123bce67fd00b90c60d9c11839
Rodrigo Silva de Sena <rodrigo.sena1163@gmail.com>
2024-10-01 12:02:48 -0400

    configuração inicial de rotas e tailwind

---
+++ src/components/header/index.tsx
@@ -0,0 +1,8 @@
+
+export function Header() {
+    return (
+        <div>
+            <h1>HEADER COMPONENT</h1>
+        </div>
+    )
+}
---
+++ src/pages/cart/index.tsx
@@ -0,0 +1,8 @@
+
+export function Cart() {
+    return (
+        <div>
+            <h1>pagina carinho de compras</h1>
+        </div>
+    )
+}
